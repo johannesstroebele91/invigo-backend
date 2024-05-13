7d5541453d56aa739c9d702d325bd6e048a7a17a
johannesstroebele91 <stroebele.j@gmail.com>
2024-05-13 16:10:15 +0200

    Add the simplest solution to fix cors problems locally

--- src/main/java/com/invigo/invigo/InvigoApplication.java
+++ src/main/java/com/invigo/invigo/InvigoApplication.java
@@ -2,15 +2,31 @@ package com.invigo.invigo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.lang.NonNull;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @RestController
 @SpringBootApplication
 public class InvigoApplication {
 
     public static void main(String[] args) {
-
         SpringApplication.run(InvigoApplication.class, args);
         System.out.println("http://localhost:8080/api/v1/products");
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:4200")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*");
+            }
+        };
     }
 }
